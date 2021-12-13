e7d5ac96fcca2e393f794e7ca13b9677b765af55
OlivierBarreau <76454614+OlivierBarreau@users.noreply.github.com>
2021-12-13 15:35:38 +0100

    continuation

---
+++ SimilationPropagationEpidemie/epidemie.js
@@ -0,0 +1,12 @@
+class Simulation {
+
+    constructor(population, NombreinfectionInitial) {
+        this.population = population;
+        var NombreinfectionInitial = NombreinfectionInitial;
+    }
+
+    start() {
+
+    }
+
+}
